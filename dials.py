cef6fc43278d63719e74375f945b006b6604a0ca
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2018-02-03 16:38:05 +0000

    Dial processing, live view

---
+++ dials.py
@@ -0,0 +1,123 @@
+import argparse
+import sys
+
+import cv2
+import time
+
+import numpy as np
+
+
+def filter_contours(contours):
+    filtered = []
+    for each in contours:
+        bb = cv2.boundingRect(each)
+        x, y, w, h = bb
+        if w < 15:
+            continue
+        if h < 15:
+            continue
+        if w > 60:
+            continue
+        if h > 60:
+            continue
+
+        rect = cv2.minAreaRect(each)
+        pos, dim, angle = rect
+        w, h = dim
+        if abs(w-h) < 8:
+            continue
+
+        filtered.append(each)
+
+    return filtered
+
+
+def process_image(img, args):
+    img = cv2.resize(img, (800, 600), interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite("%s_input.png" % args.prefix, img)
+    b, g, r = cv2.split(img)
+    cv2.imwrite("%s_green.png" % args.prefix, g)
+
+    gray = cv2.bilateralFilter(g, 5, 150, 150)
+    ret, gray = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY_INV)
+    for_circles = gray.copy()
+
+    cv2.imwrite("%s_thresh.png" % args.prefix, gray)
+    for_contours = gray.copy()
+
+    _, contours, _ = cv2.findContours(for_contours, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+    contours = filter_contours(contours)
+
+    contoured = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    # cv2.drawContours(contoured, contours, -1, (0, 255, 0))
+    for each in contours:
+        rect = cv2.minAreaRect(each)
+        x, y, angle = rect
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        contoured = cv2.drawContours(contoured, [box], 0, (0, 0, 255), 2)
+
+
+    cv2.imwrite("%s_contours.png" % args.prefix, contoured)
+
+    if args.show_images:
+        show_images(img, contoured)
+
+
+def show_images(original, contoured):
+    from matplotlib import pyplot as plt
+
+    plt.subplot(121), plt.imshow(original, cmap='gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(contoured, cmap='gray')
+    plt.title('Contours'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+
+def process_dials(image):
+    image = cv2.resize(image, (800, 600), interpolation=cv2.INTER_CUBIC)
+    b, g, r = cv2.split(image)
+    gray = cv2.bilateralFilter(g, 5, 150, 150)
+    ret, gray = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY_INV)
+    for_contours = gray.copy()
+    _, contours, _ = cv2.findContours(for_contours, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
+    contours = filter_contours(contours)
+    contoured = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(contoured, contours, -1, (0, 255, 0))
+    for each in contours:
+        rect = cv2.minAreaRect(each)
+        x, y, angle = rect
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        contoured = cv2.drawContours(contoured, [box], 0, (0, 0, 255), 2)
+
+    return contoured
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--show-images",
+        action="store_true",
+        help="Show images after processing source image"
+    )
+    parser.add_argument(
+        "-t", "--test-image",
+        help="Test an image from file, rather than capturing"
+    )
+    parser.add_argument(
+        "-p", "--prefix",
+        default="dials_" + time.strftime("%Y%m%d_%H%M%S"),
+        help="Prefix for image names"
+    )
+
+    args = parser.parse_args()
+
+    if args.test_image:
+        img = cv2.imread(args.test_image)
+        process_image(img, args)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ liveview.py
@@ -0,0 +1,49 @@
+import cv2
+from flask import render_template, Response, Flask
+
+from dials import process_dials
+
+
+class VideoCamera(object):
+    def __init__(self, processor):
+        self.video = cv2.VideoCapture(0)
+        self.processor = processor
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        image = self.processor(image)
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/dials')
+def dials():
+    camera = VideoCamera(process_dials)
+
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
