9b8ebc0e331ad8bb151b986529f0d45ac53b4035
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2018-02-04 17:04:51 +0000

    Try having different routes for different parameters for processing.

--- liveview.py
+++ liveview.py
@@ -31,6 +31,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/dials')
+def dials():
+    return render_template('dials.html')
+
+@app.route('/digits')
+def digits():
+    return render_template('digits.html')
+
 
 def gen(camera):
     while True:
@@ -39,8 +47,8 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/dials')
-def dials():
+@app.route('/dials_feed')
+def dials_feed():
     meter = HotWaterMeter()
     meter.background = "dials_threshold"
 
@@ -50,8 +58,8 @@ def dials():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-@app.route('/digits')
-def digits():
+@app.route('/digits_feed')
+def digits_feed():
     meter = HotWaterMeter()
     meter.background = "digits_threshold"
 
